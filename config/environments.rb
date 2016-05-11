6e956ca23b069bd73064a0f6d18d62834bd0b72d
Diego Lugo <diego.lugo.06@gmail.com>
2016-05-10 17:07:27 -0700

    add prod access from heroku docs

--- config/environments.rb
+++ config/environments.rb
@@ -1,4 +1,8 @@
-configure :production, :development do
+configure :development do
   db_options = YAML.load(File.read('./config/database.yml'))
   ActiveRecord::Base.establish_connection(db_options)
+end
+
+configure :production do
+  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 end
