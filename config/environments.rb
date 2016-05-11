d03ebe4d9931a23a22439baf2a7521f0093a6654
Diego Lugo <diego.lugo.06@gmail.com>
2016-05-10 17:33:27 -0700

    changed db names

--- config/environments.rb
+++ config/environments.rb
@@ -1,8 +1,12 @@
-configure :development do
-  db_options = YAML.load(File.read('./config/database.yml'))
-  ActiveRecord::Base.establish_connection(db_options)
-end
+configure :production, :development do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/dev')
 
-configure :production do
-  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+  ActiveRecord::Base.establish_connection(
+    adapter:  db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    host:     db.host,
+    username: db.user,
+    password: db.password,
+    database: db.path[1..-1],
+    encoding: 'utf8'
+  )
 end
