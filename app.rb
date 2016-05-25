f3b596b569a98a4423a6f25216217b1b62879297
Diego Lugo <diego.lugo.06@gmail.com>
2016-05-25 03:19:04 -0700

    Build simple file upload

--- app.rb
+++ app.rb
@@ -9,16 +9,32 @@ get '/' do
   erb :index
 end
 
-post '/' do
-  img_num = rand(0..1084)
-  Image.create(
-    url: "https://unsplash.it/600/500?image=#{img_num}",
-    title: "Image number #{img_num}"
-  )
-  redirect '/'
-end
+# post '/' do
+#   img_num = rand(0..1084)
+#   Image.create(
+#     url: "https://unsplash.it/600/500?image=#{img_num}",
+#     title: "Image number #{img_num}"
+#   )
+#   redirect '/'
+# end
 
 get '/photo/:id' do
   @image = Image.find(params[:id])
   erb :single
+end
+
+post '/' do
+
+  @filename = params[:file][:filename]
+  file = params[:file][:tempfile]
+
+  File.open("./public/#{@filename}", 'wb') do |f|
+    f.write(file.read)
+    Image.create(
+    url: "/" + @filename,
+    title: @filename
+    )
+  end
+
+  redirect '/'
 end
